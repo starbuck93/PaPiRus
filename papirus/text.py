2cdf85b249d8cedd732efd35dcf3063aae09206a
Adam Starbuck <me.starbuck@gmail.com>
2016-03-04 00:32:14 -0600

    Fixed text.py to actually spit text out on the screen!

--- papirus/text.py
+++ papirus/text.py
@@ -3,6 +3,7 @@ import sys
 
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageFont
 from papirus import Papirus
 import random
 
@@ -13,16 +14,18 @@ class PapirusText():
 
     def write(self, text, size = 20):
 
+        WHITE = 1
+        BLACK = 0
         # initially set all white background
-        image = Image.new('1', papirus.size, WHITE)
+        image = Image.new('1', Papirus().size, WHITE)
 
         # prepare for drawing
         draw = ImageDraw.Draw(image)
 
-        font = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', size)
+        font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', size)
 
         # Calculate the max number of char to fit on line
-        line_size = (papirus.width / (size*0.65))
+        line_size = (Papirus().width / (size*0.65))
 
         current_line = 0
         text_lines = [""]
@@ -34,9 +37,9 @@ class PapirusText():
                 text_lines[current_line] += " " + word
             else:
                 # No space left on line so move to next one
-    	        text_lines.append("")
+                text_lines.append("")
                 current_line += 1
-    	        text_lines[current_line] += " " + word
+                text_lines[current_line] += " " + word
 
         current_line = 0
         for l in text_lines:
